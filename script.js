0c0ac1f6d9c75872180d700ec05926cceb7a5e1a
hamdivazim <coding.wizard4@gmail.com>
2023-07-08 18:59:43 +0100

    Create inital homepage

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 // © Hamd Waseem 2023 under the Apache License 2.0
 // https://github.com/hamdivazim/TheLatinNet
 //
+
+function showMenu() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
